01f22c097252cb63f3343ef6c6dfc5a193cd60db
Nathan Martindale (WildfireXIII) <nmblenderdude0@gmail.com>
2016-03-11 15:43:39 -0500

    Split current NNet out into NNetOld so can work on adding in bias

---
+++ driverOld.py
@@ -0,0 +1,20 @@
+import NNetOld as net
+
+nn = net.NeuralNetwork(2,1,3,1)
+nn.initTheanoFunctions()
+#nn.readTrainingData("datasmaller.txt")
+#nn.readTrainingData("data.txt")
+nn.readTrainingData("bigdata.txt")
+
+nn.generateWeights()
+#nn.runFeedForward([[1.0,0.0]])
+#nn.backPropogate([[1.0]]);
+
+#nn.calculateRunError()
+
+nn.train()
+
+print "Initial weights:"
+nn.printInitialWeights()
+print "Final weights:"
+nn.printWeights()
